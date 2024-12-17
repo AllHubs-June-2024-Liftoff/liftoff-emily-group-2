cc970dcdfa85d5b0478a28a00ef09b43d2b5f821
Matt <beckettrichardmatthew@gmail.com>
2024-12-17 16:52:24 -0600

    imports and beginning forming classes

--- src/main/java/com/notsauce/parkd/models/AbstractEntity.java
+++ src/main/java/com/notsauce/parkd/models/AbstractEntity.java
@@ -2,7 +2,9 @@ package com.notsauce.parkd.models;
 
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
+@Getter
 @MappedSuperclass
 public abstract class AbstractEntity {
 
