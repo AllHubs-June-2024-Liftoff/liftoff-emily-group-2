215d651ec6f1b5d91e34f75d794dfa19636d7c89
Matt <beckettrichardmatthew@gmail.com>
2024-12-16 19:27:18 -0600

    commit #1

---
+++ src/main/java/com/notsauce/parkd/ParkdApplication.java
@@ -0,0 +1,13 @@
+package com.notsauce.parkd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notsauce/parkd/ParkdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notsauce.parkd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
