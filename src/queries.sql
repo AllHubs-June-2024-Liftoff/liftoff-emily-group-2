cdc90572feb18d7815f3427a8ab9ab848b043d9a
Edward King <king8three@gmailcom>
2025-01-31 15:55:30 -0500

    updated sql file

--- src/queries.sql
+++ src/queries.sql
@@ -4,13 +4,6 @@ CREATE DATABASE parkd_user_management;
 --After creating Database, USE to select database
 USE parkd_user_management;
 
---Create Table
-CREATE TABLE parkd_users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(50) NOT NULL UNIQUE,
-  
-    
-);
 
 --Create Admin user
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'P@ssword123';
@@ -18,9 +11,19 @@ GRANT ALL PRIVILEGES ON parkd_user_management.* TO 'admin'@'localhost';
 FLUSH PRIVILEGES;
 
 
-
+--Create table for capturing created/registered users data
 CREATE TABLE registered_users (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL, 
     pw_hash VARCHAR(60) NOT NULL         
 );
+
+--For injecting staged data into registered_users table
+INSERT INTO registered_users (username, pw_hash) VALUES
+('user1', 'hashed_password_1'),
+('user2', 'hashed_password_2'),
+('user3', 'hashed_password_3');
+etc...
+
+--To verify new staged data
+SELECT * FROM registered_users;
