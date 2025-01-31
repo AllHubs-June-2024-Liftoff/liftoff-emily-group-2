449b331ef4abb43280e290ef5009ac4479da9e39
Edward King <king8three@gmailcom>
2025-01-31 15:47:56 -0500

    Added follow.js in the static folder

--- src/queries.sql
+++ src/queries.sql
@@ -8,8 +8,7 @@ USE parkd_user_management;
 CREATE TABLE parkd_users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+  
     
 );
 
