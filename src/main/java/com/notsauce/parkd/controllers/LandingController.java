2e86254e752528490eb74992366937a5076cc327
evaluc <ealuckow@gmail.com>
2025-02-02 21:47:16 -0600

    Small changes to landing + landing controller

--- src/main/java/com/notsauce/parkd/controllers/LandingController.java
+++ src/main/java/com/notsauce/parkd/controllers/LandingController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Controller
 public class LandingController {
-    private /*final goes here maybe?*/ LandingService landingService;
+    private final LandingService landingService;
 
     @Autowired
     public LandingController(LandingService landingService) {
